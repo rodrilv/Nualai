1407744a2e184b4acde588dded343489bf876e22
rodrilv <rott.9954@gmail.com>
2022-05-11 14:51:57 -0500

    feat: added main tabs

---
+++ src/app/models/miembro.ts
@@ -0,0 +1,10 @@
+export class Miembro {
+    _id: String;
+    datosGenerales? : {
+        nombres: String;
+        edad: String;
+        apellido_paterno: String;
+        apellido_materno: String;
+
+    };
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Miembro } from '../models/miembro';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  
 
-  constructor() {}
+  constructor(public miembro1 : Miembro) {
+    this.miembro1 = new Miembro();
+  }
 
+  ngOnInit() {
+    this.miembro1._id="180270";
+    this.miembro1.datosGenerales.nombres = "12";
+
+    console.log(this.miembro1);
+  }
 }
