32463ce6323e66306ed0bfaad432cf74d1530c66
rodrilv <rott.9954@gmail.com>
2022-05-16 14:02:27 -0500

    feat: added hot changes to register form

--- src/app/pages/register-member/register-member.page.ts
+++ src/app/pages/register-member/register-member.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Platform } from '@ionic/angular';
+import { Miembro } from 'src/app/models/miembro';
+import { DatosGeneralesService } from 'src/app/services/datos-generales.service';
 
 @Component({
   selector: 'app-register-member',
@@ -8,13 +10,25 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./register-member.page.scss'],
 })
 export class RegisterMemberPage implements OnInit {
-  constructor(private modal: ModalController, private platform: Platform) {}
+  constructor(
+    private modal: ModalController,
+    private platform: Platform,
+    public miembro: Miembro,
+    public datosGeneralesService: DatosGeneralesService
+  ) {
+    this.miembro = new Miembro();
+  }
 
   ngOnInit() {}
 
+  async register() {
+    await this.datosGeneralesService.registerMember(this.miembro);
+    console.log(this.miembro.miembro);
+  }
+
   closeModal() {
-      this.modal.dismiss().then(() => {
-        this.modal = null;
-      });
+    this.modal.dismiss().then(() => {
+      this.modal = null;
+    });
   }
 }
