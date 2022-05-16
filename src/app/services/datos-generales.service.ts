4e62de5f0603cd3797c0e9f5057a2739232e3c20
rodrilv <rott.9954@gmail.com>
2022-05-16 13:59:13 -0500

    feat: added service for general member info

---
+++ src/app/services/datos-generales.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DatosGeneralesService {
+  devUrl: String = environment.devUrl;
+  constructor(public http: HttpClient,) {
+    
+  }
+
+  async registerMember(member: any) {
+    Swal.fire({
+      title: 'Registrando...',
+      toast: true,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+      showConfirmButton: false
+    });
+    try {
+      let subject: any = await this.http
+        .post(`${this.devUrl}registrar`, member)
+        .toPromise();
+      if (subject.ok == true) {
+        this.closeSwal(subject.ok);
+        Swal.fire({
+          title: 'Registrado Exitosamente!',
+          icon: 'success',
+          confirmButtonColor: 'green',
+          toast: true
+        });
+      } else {
+        Swal.fire({
+          title: 'Hubo un error durante la operación...',
+          icon: 'warning',
+          confirmButtonColor: 'green',
+          toast: true
+        });
+      }
+    } catch (err) {
+      this.closeSwal(true);
+      Swal.fire({
+        title: 'Hubo un error durante la operación...',
+        icon: 'warning',
+        confirmButtonColor: 'green',
+        toast: true
+      });
+    }
+  }
+
+  closeSwal(stat: boolean) {
+    if (stat == true) {
+      Swal.close();
+    } else {
+      Swal.close();
+    }
+  }
+}
