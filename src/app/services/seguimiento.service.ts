10f7687ebdf28bd5e8b33220de37f9e6a0ad262d
rodrilv <rott.9954@gmail.com>
2022-05-16 14:05:49 -0500

    feat: added service for seguimientos

---
+++ src/app/services/seguimiento.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeguimientoService {
+  miembro: any;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+}
