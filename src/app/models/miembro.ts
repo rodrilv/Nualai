4516456b0a0705da0c9777e69c2830b6e2f0ad89
rodrilv <rott.9954@gmail.com>
2022-05-16 13:57:12 -0500

    feat: added props to member model

--- src/app/models/miembro.ts
+++ src/app/models/miembro.ts
@@ -1,12 +1,16 @@
 export class Miembro {
  public miembro: any = {
-     _id: String,
      datosGenerales:{
-        nombres: String,
-        apellidos: String,
-        edad: String,
-        direccion: String,
-        telefono: String,
+        nombres: "",
+        apellidos: "",
+        edad: "",
+        genero: "",
+        domicilio: "",
+        telefono: "",
+        correo: "",
+        ocupacion: "",
+        estado_civil: "",
+        actividad_fisica: ""
      }
  }
 }
