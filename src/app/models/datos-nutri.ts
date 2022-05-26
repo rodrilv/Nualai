5abeb2976cfd50afc6b8eee7bb5893543ede5f7c
rodrilv <rott.9954@gmail.com>
2022-05-26 12:24:10 -0500

    feat: added datos nutricionales model

---
+++ src/app/models/datos-nutri.ts
@@ -0,0 +1,38 @@
+export class DatosNutriologo{
+    datosNutricionales:{
+        tratamiento_dietetico:{
+            respuesta: "",
+            hace_cuanto: ""
+        },
+        apetito: "",
+        funcionamiento_intestinal:"",
+        sintomas_gastrointestinales:{
+            respuesa:"",
+            tipo: "",
+            hace_cuanto: ""
+        },
+        tabaquismo:{
+            respuesta: "",
+            hace_cuanto: "",
+            cuanto: ""
+        },
+        alcoholismo:{
+            respuesta: "",
+            hace_cuanto: "",
+            cuanto: ""
+        },
+        H2O:{
+            cantidad:""
+        },
+        prefiere_no_consumir:[],
+        peso: "",
+        estatura: "",
+        LDL: "",
+        HDL: "",
+        IMC: "",
+        nivel_de_agua_corporal: "",
+        IMM: "",
+        edad_biológica: "",
+        peso_masa_osea: ""
+    }
+}
