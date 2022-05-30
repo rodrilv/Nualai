e398ed8e8b18344dd7d8f5837847526a9db9f4a2
rodrilv <rott.9954@gmail.com>
2022-05-30 10:47:44 -0500

    feat: added scroll to homepage

--- src/app/pages/homepage/homepage.module.ts
+++ src/app/pages/homepage/homepage.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,8 @@ import { HomepagePage } from './homepage.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomepagePageRoutingModule
+    HomepagePageRoutingModule,
+    ScrollingModule
   ],
   declarations: [HomepagePage]
 })
